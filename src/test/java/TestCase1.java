1282697b9537622cb2db6d0456f242f9d743b6bd
Anathalena <mmakevic1999@gmail.com>
2022-11-09 16:47:03 +0100

    Add tests for product buying from new page

--- src/test/java/TestCase1.java
+++ src/test/java/TestCase1.java
@@ -5,71 +5,92 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class TestCase1 {
         public WebDriver driver;
         public String baseUrl = "https://www.saucedemo.com/";
+        public WebDriverWait waitDriver;
 
         @BeforeTest
         public void setUp(){
             System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
             driver = new ChromeDriver();
             driver.get(baseUrl);
+            waitDriver = new WebDriverWait(driver, Duration.ofSeconds(5));
+            System.out.println("Browser opened");
         }
 
-        @Test
+        @Test(priority = 2)
         public void logIn(){
+            waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.id("user-name")));
             driver.findElement(By.id("user-name")).sendKeys("standard_user");
             driver.findElement(By.id("password")).sendKeys("secret_sauce");
             driver.findElement(By.id("login-button")).click();
+            waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("title")));
         }
 
-        @Test
+        @Test(priority =  3)
         public void verifyHeader(){
             String actualHeader = driver.findElement(By.className("title")).getText();
             Assert.assertEquals(actualHeader, "PRODUCTS");
+            System.out.println("Header is ok");
         }
 
-        @Test
+        @Test(priority = 4)
         public void verifyShoppingCart(){
             driver.findElement(By.id("shopping_cart_container")).isDisplayed();
+            System.out.println("Shopping cart visible");
         }
 
-        @Test
+        @Test(priority = 5)
         public void verifyBurgerMenu(){
-            driver.findElement(By.className("bm-burger-button")).isDisplayed();
+            WebElement bm_menu = driver.findElement(By.className("bm-burger-button"));
+            bm_menu.isDisplayed();
+            Assert.assertEquals(bm_menu.getLocation().getX(), 15);
+            Assert.assertEquals(bm_menu.getLocation().getY(), 20);
+            System.out.println("Burger menu in good position");
         }
 
-        @Test
+        @Test(priority = 6)
         public void verifyTwitter(){
             String url = "https://twitter.com/saucelabs";
             driver.findElement(By.xpath("//a[@href=\"" + url + "\"]")).isEnabled();
+            System.out.println("Twitter link ok");
         }
 
-        @Test
+        @Test(priority = 7)
         public void verifyFacebook(){
             String url = "https://www.facebook.com/saucelabs";
             driver.findElement(By.xpath("//a[@href=\"" + url + "\"]")).isEnabled();
+            System.out.println("Facebook link ok");
         }
 
-        @Test
+        @Test(priority = 8)
         public void verifyLinkedin(){
             String url = "https://www.linkedin.com/company/sauce-labs/";
             driver.findElement(By.xpath("//a[@href=\"" + url + "\"]")).isEnabled();
+            System.out.println("Linkedin link ok");
         }
 
-        @Test
+        @Test(priority = 9)
         public void verifyLogout(){
-            driver.findElement(By.id("logout_sidebar_link")).isEnabled();
+            driver.findElement(By.className("bm-burger-button")).click();
+            waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.id("logout_sidebar_link")));
+            driver.findElement(By.id("logout_sidebar_link")).click();
+            System.out.println("Logout link ok");
         }
 
         @AfterTest
         public void terminateBrowser(){
             driver.close();
+            System.out.println("Browser terminated");
         }
 }
--- src/test/java/TestCase2.java
+++ src/test/java/TestCase2.java
@@ -1,32 +1,57 @@
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class TestCase2 {
     public WebDriver driver;
     public String baseUrl = "https://www.saucedemo.com/";
+    public WebDriverWait waitDriver;
 
     @BeforeTest
     public void setUp(){
         System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
         driver = new ChromeDriver();
         driver.get(baseUrl);
+        waitDriver = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
-    @Test
+    @Test(priority = 1)
     public void logIn(){
+        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.id("user-name")));
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("title")));
     }
 
+    @Test(priority = 2)
+    public void checkItem(){
+        driver.findElement(By.id("item_4_title_link")).click();
+        waitDriver.until(ExpectedConditions.urlToBe("https://www.saucedemo.com/inventory-item.html?id=4"));
+        driver.findElement(By.className("inventory_details_name large_size")).isDisplayed();
+        driver.findElement(By.className("inventory_details_desc large_size")).isDisplayed();
+        driver.findElement(By.className("inventory_item_price")).isDisplayed();
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.id("remove-sauce-labs-backpack")));
 
+    }
 
-    /*@AfterTest
+    @Test(priority = 3)
+    public void goBackToProducts(){
+        driver.findElement(By.id("back-to-products")).click();
+        waitDriver.until(ExpectedConditions.urlToBe("https://www.saucedemo.com/inventory.html"));
+    }
+
+    @AfterTest
     public void terminateBrowser(){
         driver.close();
-    }*/
+    }
 }
